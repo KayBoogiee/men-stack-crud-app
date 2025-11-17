57a5eb58683ab720d7fe30c6d3528c91f4730988
KayBoogiee <Kaseemmccaleb5@gmail.com>
2025-11-17 12:28:11 +0530

    Prepare app for Heroku deployment

--- server.js
+++ server.js
@@ -4,16 +4,24 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
+// Routes
+app.get('/', (req, res) => {
+  res.send('MEN Stack CRUD App is running');
+});
 
-
-const plantRoutes = require('./routes/plants');
-app.use('/plants', plantRoutes);
-
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+// Listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
